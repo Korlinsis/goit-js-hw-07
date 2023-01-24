dd3b933c89fc9eaef88120171760766baa1c2d76
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-01-24 12:41:12 +0200

    add task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+gallery.addEventListener('click', onGalleryItemClick);
+
+function createGalleryItemsMarkup (items) {
+    return items.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    }).join('');
+}
+
+function onGalleryItemClick (e) {
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
